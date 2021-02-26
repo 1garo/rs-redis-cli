391931a290596c3aa3bb20fa5ea48b8beadddab0
1garo <alevardai@hotmail.com>
2021-02-26 18:11:51 -0300

    feat: finish redis BulkString and Array impl

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use async_std::net::{TcpStream, ToSocketAddrs};
 use async_std::io;
+use async_std::net::{TcpStream, ToSocketAddrs};
 use async_std::prelude::*;
 
-// TODO: implement basic redis cliente [https://www.youtube.com/watch?v=8TfjFZ478Rs&list=WL&index=2&t=290s&ab_channel=RyanLevick] 33:26
-// TODO: implement api that receive csv files and 
 #[async_std::main]
 async fn main() -> io::Result<()> {
-    let _stream = TcpStream::connect("localhost:6379").await?;
-    println!("Hello, world!");
+    let mut client = Client::new("localhost:6379").await?;
+    client.set("ryan".into(), "Tampa".into()).await.unwrap();
+    println!("{}", client.get("ryan".into()).await.unwrap());
     Ok(())
 }
 
@@ -23,10 +22,50 @@ fn parse_response(buffer: &[u8]) -> Result<&str, Error> {
     Ok(std::str::from_utf8(&buffer[1..buffer.len() - 2]).unwrap())
 }
 
+struct Client {
+    stream: TcpStream,
+}
+
+impl Client {
+    async fn new<A: ToSocketAddrs>(addr: A) -> Result<Client, io::Error> {
+        let stream = TcpStream::connect(addr).await?;
+        Ok(Client { stream })
+    }
+}
+
+impl Client {
+    async fn get(&mut self, key: String) -> Result<String, Error> {
+        let command = RespValue::Array(vec![
+            RespValue::BulkString(b"GET".to_vec()),
+            RespValue::BulkString(key.into_bytes()),
+        ]);
+        let mut buffer = vec![];
+        command.serialize(&mut buffer);
+        self.stream.write_all(&buffer).await?;
+
+        let bytes_read = self.stream.read(&mut buffer).await?;
+        let resp = parse_response(&buffer[..bytes_read])?;
+        Ok(resp.to_owned())
+    }
+    async fn set(&mut self, key: String, value: String) -> Result<(), Error> {
+        let command = RespValue::Array(vec![
+            RespValue::BulkString(b"SET".to_vec()),
+            RespValue::BulkString(key.into_bytes()),
+            RespValue::BulkString(value.into_bytes()),
+        ]);
+
+        let mut buffer = vec![];
+        command.serialize(&mut buffer);
+        self.stream.write_all(&buffer).await?;
+
+        let bytes_read = self.stream.read(&mut buffer).await?;
+        parse_response(&buffer[..bytes_read])?;
+        Ok(())
+    }
+}
 #[derive(Debug)]
 struct Error {}
-
-impl std::convert:From<io::Error> for Error {
+impl std::convert::From<io::Error> for Error {
     fn from(e: io::Error) -> Self {
         Error {}
     }
@@ -37,7 +76,7 @@ enum RespValue {
     Error(Vec<u8>),
     Integer(i64),
     BulkString(Vec<u8>),
-    Array(Vec<RespValue>)
+    Array(Vec<RespValue>),
 }
 
 impl RespValue {
@@ -54,17 +93,22 @@ impl RespValue {
             }
             RespValue::BulkString(mut data) => {
                 buf.push(b'$');
-                buf.append(&mut format!("{}", values.len()).into_bytes());
+                buf.append(&mut format!("{}", data.len()).into_bytes());
                 buf.push('\r' as u8);
                 buf.push('\n' as u8);
                 buf.append(&mut data);
                 buf.push('\r' as u8);
                 buf.push('\n' as u8);
-
             }
-            RespValue::SimpleString(_) => {unimplemented!()}
-            RespValue::Error(_) => {unimplemented!()}
-            RespValue::Integer(_) => {unimplemented!()}
+            RespValue::SimpleString(_) => {
+                unimplemented!()
+            }
+            RespValue::Error(_) => {
+                unimplemented!()
+            }
+            RespValue::Integer(_) => {
+                unimplemented!()
+            }
         }
     }
 }
